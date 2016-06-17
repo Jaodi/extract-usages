4a3f23dfa3ffce031ed0a969f099351e12196192
jaodi <jiaodi@bk.ru>
2016-06-18 01:29:31 +0400

    Repackaged to be installable from atom

---
+++ spec/extract-usages-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ExtractUsagesView from '../lib/extract-usages-view';
+
+describe('ExtractUsagesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
